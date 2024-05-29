4bda733e3679070020bfba6e9794b7d5930a0ccb
pgimenez96 <pablo.gimenez.zorrilla@gmail.com>
2024-05-29 19:56:19 -0400

    wip: se comete avances del curso

---
+++ src/app/dashboard/favorites/page.tsx
@@ -0,0 +1,20 @@
+import { PokemonGrid } from "@/pokemons";
+
+
+export const metadata = {
+ title: 'Favoitos',
+ description: 'Lista de favoritos',
+};
+
+export default async function PokemonsPage() {
+
+  return (
+    <div className="flex flex-col">
+
+      <span className="text-5xl my-2">Pokémons Favoritos <small className="text-blue-500">Global State</small></span>
+
+      <PokemonGrid pokemons={ [] }/>
+      
+    </div>
+  );
+}
