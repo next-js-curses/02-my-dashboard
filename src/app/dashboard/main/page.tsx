a68f8d0e1a3df17e1c321879b856c85e037ece01
pbgimenez <pgimenez@roshka.com>
2024-05-29 22:15:25 -0400

    wip: se comete avances del curso

--- src/app/dashboard/main/page.tsx
+++ src/app/dashboard/main/page.tsx
@@ -1,5 +1,19 @@
+import { SimpleWidget } from "@/components";
+
+
+export const metadata = {
+ title: 'Admin Dashboard',
+ description: 'Información general',
+};
+
 export default function MainPage() {
   return (
-    <div>Hello Main Page</div>
+    <div className="text-black p-2">
+      <h1 className="mt-2 text-3xl">Dashboard</h1>
+      <span className="text-xl">Información general</span>
+      <div className="flex flex-wrap p-2 items-center justify-center">
+        <SimpleWidget />
+      </div>
+    </div>
   )
 }
